8bc3c0eeee68319c05f219ca4fb134e001475e8d
Mr. KrY4k <nikstas056@gmail.com>
2026-01-13 21:02:57 +0500

    init

---
+++ bin/store_services_cli.dart
@@ -0,0 +1,5 @@
+import 'package:store_services_cli/store_services_cli.dart' as store_services_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${store_services_cli.calculate()}!');
+}
---
+++ test/store_services_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:store_services_cli/store_services_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
